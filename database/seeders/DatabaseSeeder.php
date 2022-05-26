473485617a5ffa02007484e9249d43341569e297
yclmrl2 <yclprint@gmail.com>
2022-05-26 17:31:23 +0300

    blade xxxx

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::insert([
+            'name' => 'ycl mrl',
+            'email' => 'yclprint@gmail.com',
+            'email_verified_at' => now(),
+            'type' => 'admin',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+
+        ]);
+         \App\Models\User::factory(5)->create();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::get('/deneme', function() {
+    return 'middleware testi';
+})->middleware(['auth','isAdmin'])->name('deneme');
+        
+    
+
+require __DIR__.'/auth.php';
